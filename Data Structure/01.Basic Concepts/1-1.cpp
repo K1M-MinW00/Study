2c4985ee536952401664ebc19c67f129abe43553
김민우 <122630746+K1M-MinW00@users.noreply.github.com>
2023-09-05 15:50:56 +0900

    1-1.cpp

---
+++ Data Structure/01.Basic Concepts/1-1.cpp
@@ -0,0 +1,23 @@
+// Q. n개의 정수 데이터를 입력 받고 입력된 데이터에서 min 값을 찾아서 출력하는 코드를 작성하라 (array 사용 X)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int min = 999, num;
+    for(int i=0;i<n;i++)
+    {
+        cin >> num;
+        if(num < min)
+        {
+            min = num;
+        }
+    }
+
+    cout << min ;
+    return 0;
+}
